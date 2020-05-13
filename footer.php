2e328d6c14ca75cee7853e14a03da3a3c8d88e9c
Matheus Decleve <me@matheus.in>
2020-05-13 15:02:17 -0300

    Adicionado header, index e footer

---
+++ footer.php
@@ -0,0 +1,6 @@
+   
+	<!-- Main JS -->
+	<script src="build/js/main.js"></script>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="theme-color" content="#000000">
+	<meta name="title" content="Matheus Decleve">
+	<meta name="description" content="">
+	<title>Matheus Decleve</title>
+
+	<!-- Meta og tags -->
+	<meta property="og:locale" content="pt_BR">
+	<meta property="og:url" content="">
+	<meta property="og:title" content="Matheus Decleve">
+	<meta property="og:site_name" content="Matheus Decleve">
+	<meta property="og:description" content="">
+	<meta property="og:type" content="website">	
+	<!-- Meta og tags - Screenshot -->
+	<meta property="og:image" content="screenshot.jpg">
+	<meta property="og:image:type" content="image/jpeg">
+	<meta property="og:image:width" content="1200">
+	<meta property="og:image:height" content="628">
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content="Matheus Decleve" />
+	<meta name="twitter:image" content="screenshot.jpg" />
+
+	<!-- Favicons -->
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="build/img/favicon-apple.png">
+	<link rel="icon" href="build/img/favicon.png" type="image/png">
+	
+	<!-- Style -->
+	<link rel="stylesheet" href="build/css/main.css">	
+</head>
+<body>
